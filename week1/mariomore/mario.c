46ac329d2981baa857a9b8e9ecc4971d3615445a
ahrynko-git <grinko.andriy@gmail.com>
2022-03-08 22:41:38 +0200

    refactoring

--- week1/mariomore/mario.c
+++ week1/mariomore/mario.c
@@ -14,12 +14,7 @@ int main(void)
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < height; j++)
-        {
-            if (j < height - i - 1)
-                printf(" ");
-            else
-                printf("#");
-        }
+            printf(j < height - i - 1 ? " " : "#");
 
         printf("  ");
 
